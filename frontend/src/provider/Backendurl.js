7cc5000ad552791ce40d524f8637b2bd5c3b6496
Samir Warsi <samirwarsi@Samirs-MacBook-Air.local>
2025-10-29 14:34:15 +0530

    changes

--- frontend/src/provider/Backendurl.js
+++ frontend/src/provider/Backendurl.js
@@ -1,1 +1,2 @@
-export const BackendUrl = "http://localhost:3000/api/v1"
+// export const BackendUrl = "http://localhost:3000/api/v1"
+export const BackendUrl = "https://highway-delight-2nzx.onrender.com/api/v1"
